da409d79576ce727bc0264d2fd7947836eb5d00a
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-11-12 20:30:53 +0600

    required packages installed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(parseInt(process.env.PORT) || 5000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
   await app.listen(3000);
 }
 bootstrap();
